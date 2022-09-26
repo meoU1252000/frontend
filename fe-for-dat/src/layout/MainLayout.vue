05147b01f664d4f0d8e9399ab5564cfe24697bdd
HuuNghia <nhn280700@gmail.com>
2022-09-26 22:45:55 +0700

    setup prj

---
+++ fe-for-dat/src/layout/MainLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main-layout">
+    <div class="header">
+      <HeaderCpn />
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import HeaderCpn from "@/components/HeaderCpn.vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+  components: { HeaderCpn },
+});
+</script>
+
+<style scoped></style>
---
+++ fe-for-dat/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main">
+    <div class="header"></div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped>
+.main {
+  min-height: 100vh;
+}
+</style>
