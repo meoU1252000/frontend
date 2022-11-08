3728332b1c8d81a7b0e993a9e970f3d37b470e55
meoU1252000 <ltdat1252000@gmail.com>
2022-11-08 13:36:03 +0700

    new

--- fe-for-dat/server.js
+++ fe-for-dat/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 const app = express();
+let server = require('http').Server(app);
 app.use(serveStatic(path.join(__dirname, 'dist')));
 app.get(/.*/, function(req,res){
     res.sendFile(path.join(__dirname,'/dist/index.html'));
 })
 
 const port = process.env.PORT || 3000;
-app.listen(port);
+server.listen(port, function() {
+    console.log("App is running on port 8000");
+});
