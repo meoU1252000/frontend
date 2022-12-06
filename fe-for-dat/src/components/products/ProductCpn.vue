0df8e11244e48cc19faae765a887a92b8c4a74ff
meoU1252000 <ltdat1252000@gmail.com>
2022-12-06 16:33:58 +0700

    fix filter

--- fe-for-dat/src/components/products/ProductCpn.vue
+++ fe-for-dat/src/components/products/ProductCpn.vue
@@ -305,15 +305,24 @@ export default defineComponent({
         try {
           listProduct.value.forEach((ele) => {
             if (ele.kind !== props.urlKind) return;
-            if (
-              ele.product_attribute[2].params == filterParam.value.color &&
-              ele.product_attribute[3].params == filterParam.value.rom ||   
-              ele.product_attribute[5].params == filterParam.value.color &&
-              ele.product_attribute[3].params == filterParam.value.rom
-            ) {
-              route.push(`/san-pham/${props.urlKind}/${ele.id}`);
-              changeSource(null);
-              throw "Nothing";
+            if(props.urlKind.includes("iphone")){
+              if (
+                ele.product_attribute[2].params == filterParam.value.color &&
+                ele.product_attribute[3].params == filterParam.value.rom 
+              ) {
+                route.push(`/san-pham/${props.urlKind}/${ele.id}`);
+                changeSource(null);
+                throw "Nothing";
+              }
+            }else if (props.urlKind.includes("macbook")){
+              if (
+                ele.product_attribute[5].params == filterParam.value.color &&
+                ele.product_attribute[3].params == filterParam.value.rom
+              ) {
+                route.push(`/san-pham/${props.urlKind}/${ele.id}`);
+                changeSource(null);
+                throw "Nothing";
+              }
             }
           });
         } catch (e) {
